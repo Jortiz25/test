d0a4c67db2521804303d0bf4a5420fa4b6f2d0b5
Jose Ortiz <joseher2511@gmail.com>
2024-03-05 14:16:30 -0500

    test

--- __test__/app.test.tsx
+++ __test__/app.test.tsx
@@ -1,18 +1,18 @@
-import { describe, expect, it } from 'vitest';
-import App from '../src/App';
+import { describe, expect, it } from 'vitest'
+import App from '../src/App'
 
 describe('App', () => {
   it('should return a JSX element', () => {
-    const result = App();
-    expect(result).toBeDefined();
-  });
+    const result = App()
+    expect(result).toBeDefined()
+  })
   it('should render the correct JSX', () => {
-    const result = App();
+    const result = App()
     const expected = (
-      <div className='w-screen h-screen flex justify-center items-center'>
-        <p className='text-3xl font-bold underline'>PSE-AVANZA</p>
+      <div className="container">
+        <p className="text-center">PSE-AVANZA</p>
       </div>
-    );
-    expect(result).toEqual(expected);
-  });
-});
+    )
+    expect(result).toEqual(expected)
+  })
+})
